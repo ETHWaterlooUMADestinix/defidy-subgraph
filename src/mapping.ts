839fa62bfbfef521902b5f2e4176dbe2d2e4d248
Sneh1999 <snehkoul1999@gmail.com>
2019-11-10 01:03:38 -0500

    graph protocol works

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,79 +1,33 @@
 import {
   GetBorrowers as GetBorrowersEvent,
   GetSuppliers as GetSuppliersEvent,
-  OwnershipRenounced as OwnershipRenouncedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  ChainlinkRequested as ChainlinkRequestedEvent,
-  ChainlinkFulfilled as ChainlinkFulfilledEvent,
-  ChainlinkCancelled as ChainlinkCancelledEvent
+  Contract
 } from "../generated/Contract/Contract"
 import {
-  GetBorrowers,
-  GetSuppliers,
-  OwnershipRenounced,
-  OwnershipTransferred,
-  ChainlinkRequested,
-  ChainlinkFulfilled,
-  ChainlinkCancelled
+  Borrower,
+  Supplier
 } from "../generated/schema"
 
 export function handleGetBorrowers(event: GetBorrowersEvent): void {
-  let entity = new GetBorrowers(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let entity = new Borrower(
+    event.transaction.hash.toHex()
   )
   entity.requestId = event.params.requestId
   entity.price = event.params.price
+  let contract = Contract.bind(event.address)
+  entity.exchangeVal =  contract.exchangeVal()
+  entity.tokenRatio = contract.tokenRatio()
   entity.save()
 }
 
 export function handleGetSuppliers(event: GetSuppliersEvent): void {
-  let entity = new GetSuppliers(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  let entity = new Supplier(
+    event.transaction.hash.toHex()
   )
   entity.requestId = event.params.requestId
   entity.price = event.params.price
-  entity.save()
-}
-
-export function handleOwnershipRenounced(event: OwnershipRenouncedEvent): void {
-  let entity = new OwnershipRenounced(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
-
-export function handleChainlinkRequested(event: ChainlinkRequestedEvent): void {
-  let entity = new ChainlinkRequested(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.id = event.params.id
-  entity.save()
-}
-
-export function handleChainlinkFulfilled(event: ChainlinkFulfilledEvent): void {
-  let entity = new ChainlinkFulfilled(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.id = event.params.id
-  entity.save()
-}
-
-export function handleChainlinkCancelled(event: ChainlinkCancelledEvent): void {
-  let entity = new ChainlinkCancelled(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.id = event.params.id
+  let contract = Contract.bind(event.address)
+  entity.exchangeVal =  contract.exchangeVal()
+  entity.tokenRatio = contract.tokenRatio()
   entity.save()
 }
